afdcba598af843bd6827981d6b8d55061657c31e
shabirbaloch398 <63488186+shabirbaloch398@users.noreply.github.com>
2020-04-17 21:39:25 +0500

    Update balochAnonaymous.py

--- balochAnonaymous.py
+++ balochAnonaymous.py
@@ -66,7 +66,7 @@ BALOCH HACKER 💯
 BALOCH KAOM🔴🔴
 FAN OF JUNAID JAMSHED ⏏️⏏️
 PAKISTAN ZINDABAD🇵🇰🇵🇰🇵🇰🇵🇰🇵🇰
-WE NEVER FORGER WE NEVER FORGIVE
+WE NEVER FORGET WE NEVER FORGIVE
 PAK HACKER🇵🇰🇵🇰🇵🇰🇵🇰
 Monospace
 View Against Background: ▼
@@ -152,7 +152,7 @@ print  """
 \033[1;94mFacebook\033[1;91m: \033[1;91mbalochhacker
 \033[1;94mWhatsapp\033[1;91m: \033[1;91m+923232132362
 \033[1;95m♡╰──────────•◈•──────────╯♡\033[1;96mShabirBaloch\033[1;95m♡╰──────────•◈•──────────╯♡"""
-jalan('              \033[1;96m....................BlackMafia.....................:')
+jalan('              \033[1;96m....................Shabir Baloch Hacker.....................:')
 jalan("\033[1;93m   ┈┈┈┈┈┈┈┈╱▔▔▔▔╲┈┈┈┈┈┈┈┈   ")
 jalan('\033[1;93m   ┈┈┈┈┈┈┈▕▕╲┊┊╱▏▏┈┈┈┈┈┈┈   ')
 jalan('\033[1;93m   ┈┈┈┈┈┈┈▕▕▂╱╲▂▏▏┈┈┈┈┈┈┈   ')
@@ -189,7 +189,7 @@ def login():
 		print logo
 		jalan(' \033[1;92mWarning: \033[1;97mDo Not Use Your Personal Account' )
 		jalan(' \033[1;92m   Note: \033[1;97mUse a New Account To Login' )
-		print "\033[1;95m♡──────────•◈•──────────♡\033[1;96mBlackMafia\033[1;95m♡──────────•◈•──────────♡"
+		print "\033[1;95m♡──────────•◈•──────────♡\033[1;96mShabirBaloch\033[1;95m♡──────────•◈•──────────♡"
 		print('	   \033[1;94m♡\x1b[1;91m》》》》》》LOGIN WITH FACEBOOK《《《《《《\x1b[1;94m♡' )
 		print('	' )
 		id = raw_input('\033[1;96m[+] \x1b[1;92mID/Email\x1b[1;95m: \x1b[1;96m')
